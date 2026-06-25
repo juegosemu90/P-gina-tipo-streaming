cb6accd57a1e4eb7a6fb7c1faa92a0426d6cee7e
juegosemu90 <juegosemu90@users.noreply.github.com>
2026-06-25 20:31:48 +0000

    deploy: 21964a8d94bec81792a644fe5e0f7d6960408aa0

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Service worker simple para que RosaTV pueda instalarse como app (PWA).
+const CACHE_NAME = 'rosatv-cache-v1'
+
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone()
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy))
+        return response
+      })
+      .catch(() => caches.match(event.request))
+  )
+})
